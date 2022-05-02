7b6df30e3539428e7af1c293dc96eb0473522295
Tan Le <tanle@Tans-MBP.hsd1.fl.comcast.net>
2022-05-02 08:56:41 -0400

    i dont know what to do

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Basic portfolio about myself, background info, projects and interests
 Welcome to the my personal portfolio webpage.
 This is where you can get to learn more about me professionally and personally wise including projects that I had my hands on to demonstrate my knowledge and understanding of software engineer programming languages and skills.
+The portfolio was created by dividing the page into sections as scroll downs.
+I built each portion from top to bottom by designing each feature separately. 
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ $('.navigation').hover(function(){
 // I searched up jquery scroll function, results are from jquery.com
 $(document).ready(function() {
   $(window).scroll(function() {
-    if(this.scrollY > 30) {
+    if(this.scrollY > 20) {
       $('.navigation').addClass('navigation-hover')
     }
     else {
@@ -23,5 +23,33 @@ $(document).ready(function(){
   $("#icon").click(function () {
     $('.slide-menu').slideToggle('slow')
   })
+})
+const modal = () => {
+  $('.modal-info').toggle('slow')
+}
+$('#resume-icon').on('click',modal)
+
+let img = 0
+const num = $('.carousel-pictures').children().length - 1
+$('right-button').on('click', () => {
+  $('.carousel-pictures').children().eq(img).css('display','none')
+  if(img<num) {
+    img++
+  }
+  else(
+    img = 0
+  )
+  $('.carousel-pictures').children().eq(img).css('display','block')
+})
+
+$('left-button').on('click', () => {
+  $('.carousel-pictures').children().eq(img).css('display','none')
+  if(img>0) {
+    img--
+  }
+  else(
+    img = num
+  )
+  $('.carousel-pictures').children().eq(img).css('display','block')
 })
 })
